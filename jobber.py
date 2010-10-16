811c323ce0d8036267e10690e35549870c7e96a8
David Ackerman <dackerman@google.com>
2010-10-16 13:54:33 -0700

    initial commit

---
+++ jobber.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+jobber.py
+
+Created by David Ackerman on 2010-10-16.
+Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+  main()
+
