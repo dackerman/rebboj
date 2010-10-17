165af8abc809ebdcaf6f5a6bb4badfac30519247
Sweta <sweta@sweta-laptop.(none)>
2010-10-16 20:39:56 -0700

    Fixed controllers import and added styles to app yaml

--- controllers.py
+++ controllers.py
@@ -16,6 +16,7 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
+from models.review import Review
 
 class JobberHomeController(webapp.RequestHandler):
     def get(self):
