255d27af82406c9473619bf67f2524e11850c03b
David Ackerman <david.w.ackerman@gmail.com>
2010-10-19 00:39:10 -0700

    Added local star images and some more css styling

--- public/javascript/stars.js
+++ public/javascript/stars.js
@@ -1,5 +1,5 @@
-var offImage = "https://audience.withoutabox.com/images/star-off.jpg";
-var onImage = "https://audience.withoutabox.com/images/star-on.jpg";
+var offImage = "/images/star-off.jpg";
+var onImage = "/images/star-on.jpg";
 
 var Stars = function() { };
 gStarId = 0;
@@ -11,9 +11,8 @@ Stars.createStars = function(name){
   for(var i = 1; i <= 5; i++){
     var star = document.createElement('img');
     $(starContainer).append(star);
-    star.id = gStarId;
+    star.id = "__starid" + gStarId;
     var starid = "#" + star.id;
-    gStarId++;
     star.name = 'star_' + i;
     star.src = offImage;
     $(starid).data('ele', name);
@@ -25,5 +24,6 @@ Stars.createStars = function(name){
         $(this).nextAll().attr('src', offImage);
         $($(this).data('ele')).attr('value',$(this).data('position'));
       });
+    gStarId++;
   }
 };
